ad95e12ab90fdf252b3018e97813456788fd0f8b
Jack <Jack@moving.com>
2024-04-26 16:10:35 +0800

    push commit

--- src/gitdemo.java
+++ src/gitdemo.java
@@ -7,5 +7,6 @@ public class gitdemo {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push to github test");
+        System.out.println("push to github test with ssh");
     }
 }

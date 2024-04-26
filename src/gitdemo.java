c4aabaf3d086bcb33923669ffae3ab94f511ee8e
Jack <Jack@moving.com>
2024-04-26 16:08:26 +0800

    master commit3

--- src/gitdemo.java
+++ src/gitdemo.java
@@ -6,5 +6,6 @@ public class gitdemo {
         System.out.println("hello world");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push to github test");
     }
 }
